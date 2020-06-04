824aa7da4d86318f51d12775a29ed20a7733c864
rafdes <raffaele.desantis@cgn.it>
2020-06-04 11:15:34 +0200

    Fine esercizi

--- recfun/src/main/scala/recfun/RecFun.scala
+++ recfun/src/main/scala/recfun/RecFun.scala
@@ -14,15 +14,43 @@ object RecFun extends RecFunInterface {
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = ???
+  def pascal(c: Int, r: Int): Int = {
+    if (c==0 || c==r) 1
+    else pascal(c-1, r-1) + pascal(c, r-1)
+  }
 
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+
+    /** V2 - funzionante, uso la lista direttamente */
+    def balanceEsegui(appChars:List[Char],numPar:Int=0):Boolean = {
+      if (numPar<0) false
+      else
+        appChars match {
+          case Nil     => numPar==0
+          case '('::xs => balanceEsegui(appChars.tail, numPar+1)
+          case ')'::xs => balanceEsegui(appChars.tail, numPar-1)
+          case _::xs   => balanceEsegui(appChars.tail, numPar)
+        }
+    }
+
+    balanceEsegui(chars)
+  }
 
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int = {
+    /** No gettoni/no schei */
+    if (coins.isEmpty || money<0) 0
+    else
+      /** Taglio di soldi perfetto */
+      if (money==0) 1
+      else
+        /** cambio con il primo taglio e sottraggo il taglio al numero,
+         *  richiamo la funzione sui tagli successivi  con il numero per intero */
+        countChange(money-coins(0), coins) + countChange(money, coins.tail)
+  }
 }
